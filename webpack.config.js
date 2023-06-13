579d9de69f6c50d4d62a63e6c584afc54c840df5
Balolebwami <tchasingajacques@gmail.com>
2023-06-14 01:37:08 +0200

    new changing

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'Running.js',
-    // clean: true,
+    clean: true,
   },
   devServer: {
     static: './dist',
