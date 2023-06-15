d839f114d4e376711e30e8e9ef502b21766dd9d0
Balolebwami <tchasingajacques@gmail.com>
2023-06-15 15:56:55 +0200

    updating change's

--- src/index.js
+++ src/index.js
@@ -3,47 +3,49 @@ import Store from './modules/Store.js';
 import Book from './modules/Book.js';
 import './style/styles.css';
 
-document.addEventListener('DOMContentLoaded', UI.displayBooks);
+document.addEventListener('DOMContentLoaded', () => {
+  UI.displayBooks();
 
-document.querySelector('#book-form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const title = document.querySelector('#title').value;
-  if (title !== '') {
-    const book = new Book(title);
-    UI.addBookToList(book);
-    Store.addBook(book);
-    document.querySelector('#title').value = '';
-  }
-});
+  document.querySelector('#book-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const title = document.querySelector('#title').value;
+    if (title !== '') {
+      const book = new Book(title);
+      UI.addBookToList(book);
+      Store.addBook(book);
+      document.querySelector('#title').value = '';
+    }
+  });
 
-document.querySelector('.book-list').addEventListener('click', (e) => {
-  if (e.target.classList.contains('delete')) {
-    UI.deleteBook(e.target);
-    const { isbn } = e.target.parentElement.parentElement.dataset;
-    Store.removeBook(isbn);
-  } else if (e.target.classList.contains('mybox')) {
-    const row = e.target.parentElement.parentElement;
-    row.classList.toggle('completed');
-    const { isbn } = row.dataset;
-    Store.toggleComplete(isbn);
-  }
-});
+  document.querySelector('.book-list').addEventListener('click', (e) => {
+    if (e.target.classList.contains('delete')) {
+      UI.deleteBook(e.target);
+      const { isbn } = e.target.parentElement.parentElement.dataset;
+      Store.removeBook(isbn);
+    } else if (e.target.classList.contains('mybox')) {
+      const row = e.target.parentElement.parentElement;
+      row.classList.toggle('completed');
+      const { isbn } = row.dataset;
+      Store.toggleComplete(isbn);
+    }
+  });
 
-document.querySelector('.book-list').addEventListener('input', (e) => {
-  if (e.target.classList.contains('Edit')) {
-    const { isbn } = e.target.parentElement.parentElement.dataset;
-    const newTitle = e.target.value;
-    UI.editBookTitle(isbn, newTitle);
-    Store.editBookTitle(isbn, newTitle);
-  }
-});
+  document.querySelector('.book-list').addEventListener('input', (e) => {
+    if (e.target.classList.contains('Edit')) {
+      const { isbn } = e.target.parentElement.parentElement.dataset;
+      const newTitle = e.target.value;
+      UI.editBookTitle(isbn, newTitle);
+      Store.editBookTitle(isbn, newTitle);
+    }
+  });
 
-document.querySelector('.ClearAll').addEventListener('click', () => {
-  UI.clearCompletedBooks();
-  Store.clearCompletedBooks();
-});
+  document.querySelector('.ClearAll').addEventListener('click', () => {
+    UI.clearCompletedBooks();
+    Store.clearCompletedBooks();
+  });
 
-document.querySelector('.ClearAll').addEventListener('click', () => {
-  UI.removeAllBooks();
-  Store.removeAllBooks();
+  document.querySelector('.ClearAll').addEventListener('click', () => {
+    UI.removeAllBooks();
+    Store.removeAllBooks();
+  });
 });
