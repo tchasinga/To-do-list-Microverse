57d74393c8d765d588c029578028579c460e6544
Balolebwami <tchasingajacques@gmail.com>
2023-06-15 14:28:14 +0200

    new commit

--- src/modules/Book.js
+++ src/modules/Book.js
@@ -1,7 +1,7 @@
 class Book {
   constructor(title) {
     this.title = title;
-    this.isbn = Date.now().toString(); // Generate a unique identifier for each book
+    this.isbn = Date.now().toString(); // Generate a unique identifier for each book and manager it
     this.completed = false;
   }
 }
